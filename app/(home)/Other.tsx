9ba5bba1e6d2865f7c2646136cc3a4a59b498fb0
anzarbk <ansarzk4@gmail.com>
2023-09-04 19:37:30 +0530

    travel layout complete

---
+++ app/(home)/Other.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {}
+
+const Other = (props: Props) => {
+    return (
+      <section>
+    <hr className='border-1' />
+          {/* HEADER */}
+              <p className='font-bold text-2xl'>Other trending posts</p>
+            <div className='flex items-center gap-3 my-8'>
+                
+          </div>           
+      </section>
+  )
+}
+
+export default Other
